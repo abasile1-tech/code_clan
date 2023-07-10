cf943d1bb67534b03c1392ec7fb27f6d64ea2f42
John Basile <ashton.basile@gmail.com>
2023-07-10 13:33:00 +0100

    I added some protection to the data members

--- code_clan_work/week_02/day_1/bank_account.py
+++ code_clan_work/week_02/day_1/bank_account.py
@@ -1,27 +1,40 @@
 class BankAccount():
 	def __init__(self, holder_name, balance, account_type):
-		self.holder_name = holder_name
-		self.balance = balance
-		self.account_type = account_type
-		self.rates = {
+		self.__holder_name = holder_name
+		self.__balance = balance
+		self.__account_type = account_type
+		self.__rates = {
 			"personal": 50,
 			"business": 100,
 			"charity": 0
 		}
 
+	@property
+	def holder_name(self):
+		return self.__holder_name
+	@property
+	def balance(self):
+		return self.__balance
+	@property
+	def account_type(self):
+		return self.__account_type
+	@property
+	def rates(self):
+		return self.__rates
+
 	def get_holder_name(self):
-		return self.holder_name
+		return self.__holder_name
 	def get_balance(self):
-		return self.balance
+		return self.__balance
 	def get_account_type(self):
-		return self.account_type
+		return self.__account_type
 
 	def set_holder_name(self, holder_name):
-		self.holder_name = holder_name
+		self.__holder_name = holder_name
 	def set_balance(self, balance):
-		self.balance = balance
+		self.__balance = balance
 	def set_account_type(self, account_type):
-		self.account_type = account_type
+		self.__account_type = account_type
 
 	def pay_in(self, amount):
 		current_balance = self.get_balance()
