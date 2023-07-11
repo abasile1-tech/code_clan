1413edf56868bc4b8c68efcda9cdf6cee3470848
John Basile <ashton.basile@gmail.com>
2023-07-11 15:46:51 +0100

    starter for the task_loops assignment

---
+++ code_clan_work/week_02/day_2/task_loops.py
@@ -0,0 +1,21 @@
+# Exercise 1
+# Using single list comprehension, and the following list:
+# ages = [5, 15, 64, 27, 84, 26]
+# Return a list of only the odd ages.
+# Exercise 2
+# Using single list comprehension, and the following list:
+# years = range(2000, 2100)
+# Return a list of the leap years in this century.
+# Hint: A leap year is one that can be exactly divided by 4 (such as 2016, 2020, 2024, etc), except if it can be exactly divided by 100.
+# Exercise 3
+# Using single list comprehension, and the following list:
+# chicken_names = ["Hen Solo", "Cluck Norris", "Hennifer Lopez", "ChewPekka", "Feather Locklear"]
+# Return a list with chickens whose name is more than 10 characters
+# Return a list of chickens whose name starts with the letter H
+# Exercise 4
+# Using a single list comprehension, and the following list:
+# words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+# Build a new list, with the first letter from each word
+# Convert each letter to lower case
+# Expected output: ["t", "q", "b", "f", "j", "o", "t", "l", "d"]
+# Hint: Strings in Python work as if they were a tuple full of characters. How would you get the first element from a tuple or list?
