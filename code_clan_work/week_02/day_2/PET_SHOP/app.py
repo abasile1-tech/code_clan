f905a0ef9a7515bc1498e248ab7f3e6ab2046475
John Basile <ashton.basile@gmail.com>
2023-07-11 11:10:37 +0100

    I added some more classwork with the PetShop class

--- code_clan_work/week_02/day_2/PET_SHOP/app.py
+++ code_clan_work/week_02/day_2/PET_SHOP/app.py
@@ -7,3 +7,13 @@ mrs_norris = Pet("Mrs. Norris", "Cat", "Maine Coon", 100)
 pets = [monty, charles, mrs_norris]
 
 cc_pet_shop = PetShop("CodeClan Pet Shop", 0, pets)
+num_of_pets = cc_pet_shop.stock_count()
+print(f"Shop has {num_of_pets} pets in stock")
+
+cc_pet_shop.increase_total_cash(100)
+print(f"Shop now has £{cc_pet_shop.cash}")
+
+cc_pet_shop.remove_pet(mrs_norris)
+print(f"shop now has {cc_pet_shop.stock_count()}")
+
+print(cc_pet_shop.find_pet_by_name("Monty"))
--- code_clan_work/week_02/day_2/PET_SHOP/pet_shop.py
+++ code_clan_work/week_02/day_2/PET_SHOP/pet_shop.py
@@ -3,3 +3,21 @@ class PetShop:
 		self.name = name
 		self.cash = cash
 		self.pets = pets
+		self.pets_sold = 0
+
+	def stock_count(self):
+		return len(self.pets)
+
+	def increase_total_cash(self, amount):
+		self.cash += amount
+
+	def add_pet(self, pet):
+		self.pets.append(pet)
+
+	def remove_pet(self, pet):
+		self.pets.remove(pet)
+
+	def find_pet_by_name(self, name):
+		for pet in self.pets:
+			if pet.name == name:
+				return pet
